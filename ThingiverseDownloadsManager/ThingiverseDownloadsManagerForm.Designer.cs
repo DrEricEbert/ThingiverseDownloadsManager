27ee3594293538fbf7106b6fcd35f847b04355bb
DrEricEbert <34137689+DrEricEbert@users.noreply.github.com>
2023-03-25 15:14:38 +0100

    Add files via upload

---
+++ ThingiverseDownloadsManager/ThingiverseDownloadsManagerForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ThingiverseDownloadsManager
+{
+    partial class ThingiverseDownloadsManagerForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verzeichnisAuswählenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listViewZips = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listViewImages = new System.Windows.Forms.ListView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.entpackenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.entpackenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verzeichnisAuswählenToolStripMenuItem,
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // verzeichnisAuswählenToolStripMenuItem
+            // 
+            this.verzeichnisAuswählenToolStripMenuItem.Name = "verzeichnisAuswählenToolStripMenuItem";
+            this.verzeichnisAuswählenToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.verzeichnisAuswählenToolStripMenuItem.Text = "Verzeichnis auswählen";
+            this.verzeichnisAuswählenToolStripMenuItem.Click += new System.EventHandler(this.verzeichnisAuswählenToolStripMenuItem_Click);
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 30);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewZips);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 420);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listViewZips
+            // 
+            this.listViewZips.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewZips.HideSelection = false;
+            this.listViewZips.Location = new System.Drawing.Point(0, 0);
+            this.listViewZips.MultiSelect = false;
+            this.listViewZips.Name = "listViewZips";
+            this.listViewZips.ShowItemToolTips = true;
+            this.listViewZips.Size = new System.Drawing.Size(266, 420);
+            this.listViewZips.TabIndex = 0;
+            this.listViewZips.UseCompatibleStateImageBehavior = false;
+            this.listViewZips.View = System.Windows.Forms.View.List;
+            this.listViewZips.SelectedIndexChanged += new System.EventHandler(this.listViewZips_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listViewImages
+            // 
+            this.listViewImages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewImages.HideSelection = false;
+            this.listViewImages.Location = new System.Drawing.Point(0, 0);
+            this.listViewImages.Name = "listViewImages";
+            this.listViewImages.Size = new System.Drawing.Size(530, 113);
+            this.listViewImages.TabIndex = 0;
+            this.listViewImages.TileSize = new System.Drawing.Size(256, 256);
+            this.listViewImages.UseCompatibleStateImageBehavior = false;
+            this.listViewImages.SelectedIndexChanged += new System.EventHandler(this.listViewImages_SelectedIndexChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listViewImages);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(530, 420);
+            this.splitContainer2.SplitterDistance = 113;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(530, 231);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // entpackenToolStripMenuItem
+            // 
+            this.entpackenToolStripMenuItem.Name = "entpackenToolStripMenuItem";
+            this.entpackenToolStripMenuItem.Size = new System.Drawing.Size(91, 26);
+            this.entpackenToolStripMenuItem.Text = "Entpacken";
+            this.entpackenToolStripMenuItem.Click += new System.EventHandler(this.entpackenToolStripMenuItem_Click);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.pictureBox);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.textBox);
+            this.splitContainer3.Size = new System.Drawing.Size(530, 303);
+            this.splitContainer3.SplitterDistance = 231;
+            this.splitContainer3.TabIndex = 1;
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(0, 0);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(530, 68);
+            this.textBox.TabIndex = 0;
+            // 
+            // ThingiverseDownloadsManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ThingiverseDownloadsManagerForm";
+            this.Text = "Thingiverse Downloads Manager by Eric Ebert 2023";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            this.splitContainer3.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verzeichnisAuswählenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listViewZips;
+        private System.Windows.Forms.ListView listViewImages;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ToolStripMenuItem entpackenToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ ThingiverseDownloadsManager/ThingiverseDownloadsManagerForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using Ionic.Zip;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ThingiverseDownloadsManager
+{
+    public partial class ThingiverseDownloadsManagerForm : Form
+    {
+        public ThingiverseDownloadsManagerForm()
+        {
+            try
+            {
+                Directory.Delete("images", true);
+            }catch(SystemException) 
+            { }
+            InitializeComponent();
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void verzeichnisAuswählenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = "C:\\Data\\stl";
+            if (fbd.ShowDialog() != DialogResult.OK) return;
+            string[] zips = Directory.GetFiles(fbd.SelectedPath, "*.zip",SearchOption.AllDirectories);
+            listViewZips.BeginUpdate();
+            foreach (string zip in zips) 
+            {
+                ListViewItem item= listViewZips.Items.Add(Path.GetFileNameWithoutExtension(zip));
+                item.Tag = zip;
+            }
+            listViewZips.EndUpdate();
+        }
+
+        private void listViewZips_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imageList1.Images.Clear();
+            imageList1.ImageSize = new Size(128, 128);
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            listViewImages.Clear();
+            try
+            {
+                Directory.Delete("images", true);
+            }
+            catch (SystemException)
+            { }
+            listViewImages.LargeImageList = imageList1;
+            if (listViewZips.SelectedItems.Count != 1) return;
+            ListViewItem item = listViewZips.SelectedItems[0];
+            string path = (string)item.Tag;
+            
+            using (ZipFile zip = ZipFile.Read(path))
+            {
+                bool result = zip.ContainsEntry("README.txt");
+                if (result)
+                {
+                    foreach (var entry in zip.Entries)
+                    {
+                        if (entry.FileName.Contains("images/")) 
+                        {
+                            entry.Extract(ExtractExistingFileAction.DoNotOverwrite);
+                            Bitmap i = new Bitmap(entry.FileName);
+                            imageList1.Images.Add(entry.FileName, i);
+                            var listViewItem = listViewImages.Items.Add(entry.FileName);
+                            // and tell the item which image to use
+                            listViewItem.ImageKey = entry.FileName;
+                            listViewItem.Tag = i;
+
+
+                        }
+                        if (entry.FileName.Contains("README.txt")) 
+                        {
+                            entry.Extract(ExtractExistingFileAction.OverwriteSilently);
+                            TextReader tr = new StreamReader("README.txt");
+                            string txt = tr.ReadToEnd();
+                            tr.Close();
+                            textBox.Text=txt;
+                        }
+                    }
+                    //zip.ExtractAll(target, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+            listViewImages.LargeImageList = imageList1;
+           // 
+        }
+
+        private void listViewImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewImages.SelectedItems.Count != 1) return;
+             Bitmap bmp = (Bitmap)listViewImages.SelectedItems[0].Tag;
+            this.pictureBox.Image = bmp;
+        }
+
+        private void entpackenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listViewZips.SelectedItems.Count != 1) return;
+            ListViewItem item = listViewZips.SelectedItems[0];
+            string path = (string)item.Tag;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = "C:\\Data\\stl";
+            if (fbd.ShowDialog() != DialogResult.OK) return;
+
+            using (ZipFile zip = ZipFile.Read(path))
+            {
+               zip.ExtractAll(fbd.SelectedPath, ExtractExistingFileAction.OverwriteSilently);
+            }
+        }
+    }
+}
